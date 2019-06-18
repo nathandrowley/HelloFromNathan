2d5f2c03d0a8c562f61b9628bf2469dfc4378243
Grayson <grayclimber@gmail.com>
2019-06-18 09:34:54 -0600

    i typed stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	//My Name is nathan rowley
     //testing testing testing
         System.out.println("Helllo this is nathan");
+        System.out.println("Helllo this is Grayson,  your computer will shut down in  5, 4, 3, 2, 1 .....");
     }
 }
