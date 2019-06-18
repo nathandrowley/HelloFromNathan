53a53601e5c60e668afd45e387e8ad85441afb77
nathan rowley <row16016@byui.edu>
2019-06-18 09:20:33 -0600

    added print line with name.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	//My Name is nathan rowley
     //testing testing testing
+        System.out.println("Helllo this is nathan");
     }
 }
